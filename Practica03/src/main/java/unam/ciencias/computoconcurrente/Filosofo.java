257e8a6cd42bbec05ae7ff510e67867803c66586
Jose117053 <adolfo19420@gmail.com>
2024-03-17 10:53:30 -0600

    Practica 3 terminada

--- Practica03/src/main/java/unam/ciencias/computoconcurrente/Filosofo.java
+++ Practica03/src/main/java/unam/ciencias/computoconcurrente/Filosofo.java
@@ -20,17 +20,52 @@ public class Filosofo implements Runnable {
 
     public void run() {
         //Aqui va tu codigo
+
+        try {
+            while (!haComido()) {
+                pensar();
+                System.out.println();
+                tomarPalillos();
+                comer();
+                soltarPalillos();
+                System.out.println();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("esto no deberia pasar: " );
+            e.printStackTrace();
+        }
     }
 
     private void pensar() throws InterruptedException {
-        //Aqui va tu codigo
+        System.out.println("Filósofo " + id + " está pensando...");
+        Thread.sleep((long) (Math.random() * 2000));
     }
 
     private void comer() throws InterruptedException {
-        //Aqui va tu codigo
+        System.out.println("Filósofo " + id + " entra en su sección crítica (comienza a comer).");
+        System.out.println("Filósofo " + id + " está comiendo.");
+        Thread.sleep((long) (Math.random() * 2000));
     }
 
     public boolean haComido() {
         return haComido;
     }
+
+    private void tomarPalillos() throws InterruptedException {
+        palilloIzquierdo.acquire();
+        System.out.println("Filósofo " + id + " tomó el tenedor izquierdo.");
+        palilloDerecho.acquire();
+        System.out.println("Filósofo " + id + " tomó el tenedor derecho.");
+    }
+
+
+    private void soltarPalillos() {
+        palilloIzquierdo.release();
+        System.out.println("Filósofo " + id + " ha terminado de comer");
+        System.out.println("Filósofo " + id + " soltó el tenedor izquierdo.");
+        palilloDerecho.release();
+        System.out.println("Filósofo " + id + " soltó el tenedor derecho.");
+        haComido = true; // para que se ejecute al final
+    }
+
 }
