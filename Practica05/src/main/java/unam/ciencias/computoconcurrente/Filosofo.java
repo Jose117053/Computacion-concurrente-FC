ab9693ff9c8415bc24069f5af8462c853de15e80
jose117053 <adolfo19420@gmail.com>
2024-04-22 19:22:06 -0600

    Modificacion a impresion de colores

--- Practica05/src/main/java/unam/ciencias/computoconcurrente/Filosofo.java
+++ Practica05/src/main/java/unam/ciencias/computoconcurrente/Filosofo.java
@@ -1,8 +1,5 @@
 package unam.ciencias.computoconcurrente;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Cada filósofo se ejecuta en un hilo.
  */
@@ -10,20 +7,15 @@ public class Filosofo implements Runnable {
     private int id;
     private Object palilloIzquierdo, palilloDerecho;
     private boolean haComido;
-    private static final Map<Integer, String> colores = new HashMap<>();
-    static {
-        colores.put(0, "\u001B[31m"); // cyan
-        colores.put(1, "\u001B[32m"); // Verde
-        colores.put(2, "\u001B[33m"); // Amarillo
-        colores.put(3, "\u001B[34m"); // Azul
-        colores.put(4, "\u001B[35m"); // Morado
-    }
+    private final String color;
+
 
     public Filosofo(int id, Object palilloIzquierdo, Object palilloDerecho) {
         this.id = id;
         this.palilloIzquierdo = palilloIzquierdo;
         this.palilloDerecho = palilloDerecho;
         this.haComido = false;
+        this.color = "\033[3" + (id % 8 + 1) + "m";
     }
 
     public void run() {
@@ -31,19 +23,19 @@ public class Filosofo implements Runnable {
         try {
             while (!haComido) {
                 pensar();
-                System.out.println(colores.get(id));
+                System.out.println(color);
                 comer();
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            System.out.println();
+            System.out.println("Esto no deberia pasar");
         }
     }
 
     private void pensar() throws InterruptedException {
         //Aqui va tu codigo
-        System.out.println(colores.get(id) +"Filósofo " + id + " está pensando...");
-        Thread.sleep((long)(Math.random() * 1000)); // Pensar durante un tiempo aleatorio
+        System.out.println(color +"Filósofo " + id + " está pensando...");
+        Thread.sleep((long)(Math.random() * 1000));
     }
 
     /**
@@ -55,20 +47,20 @@ public class Filosofo implements Runnable {
         //Aqui va tu codigo
         synchronized (palilloIzquierdo) {
 
-            System.out.println(colores.get(id)+"Filósofo " + id + " tomó el tenedor izquierdo.");
+            System.out.println(color +"Filósofo " + id + " tomó el tenedor izquierdo.");
 
             synchronized (palilloDerecho) {
 
-                System.out.println(colores.get(id) +"Filósofo " + id + " tomó el tenedor derecho.");
-                System.out.println(colores.get(id) +"Filósofo " + id + " entra en su sección crítica (comienza a comer).");
-                System.out.println(colores.get(id) +"Filósofo " + id + " está comiendo.");
+                System.out.println(color +"Filósofo " + id + " tomó el tenedor derecho.");
+                System.out.println(color +"Filósofo " + id + " entra en su sección crítica (comienza a comer).");
+                System.out.println(color +"Filósofo " + id + " está comiendo.");
                 Thread.sleep(1000);
                 haComido = true;
-                System.out.println(colores.get(id) +"Filósofo " + id + " soltó el tenedor derecho.");
+                System.out.println(color +"Filósofo " + id + " soltó el tenedor derecho.");
 
             }
 
-            System.out.println(colores.get(id) +"Filósofo " + id + " soltó el tenedor izquierdo.\n");
+            System.out.println(color +"Filósofo " + id + " soltó el tenedor izquierdo.\n");
 
         }
     }
